310b2d48e31292ce46cba96fbaaaff8a0d584618
jh5H <ojh2079@rlmodel.com>
2024-07-09 04:29:38 +0900

    added launch file

---
+++ launch/joy_to_cmd.launch.py
@@ -0,0 +1,24 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+
+    joy_node =  Node(
+            package='joy',
+            executable='joy_node',
+            name='joy')
+    
+    joystick_node = Node(
+            package='joystick_py',
+            executable='joy_0709',
+            # name='JoyToCmd1',
+            remappings=[('cmd_vel', 'turtle1/cmd_vel')]
+    )
+
+
+    return LaunchDescription([
+        joy_node,
+        joystick_node
+
+  ])
